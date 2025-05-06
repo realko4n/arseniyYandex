44a068a34ee50b0bbed9eb164da2a0d6a0360d90
realko4n <90069252+realko4n@users.noreply.github.com>
2025-05-06 20:53:27 +0300

    6.05.25

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from spacy.lang.ru.stop_words import STOP_WORDS
 
 model = ru_core_news_md.load()
 text = model("Классный фильм, хорошие акетры, супер сюжет")
-text_list = [word for word in text]
+text_list = [word.lemma_ for word in text]
 print(text_list)
+
+filter_text_list = [elem for elem in text_list if not elem in STOP_WORDS]
+print(filter_text_list)
 
